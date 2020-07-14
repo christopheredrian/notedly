a5220759fcf797a0ba36395f51345dd424553a62
Christopher Espiritu <christopheredrian@gmail.com>
2020-07-13 20:12:51 -0400

    user: signin signup, authorization

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
 // index.js
 // This is the main entry point of our application
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('./schema');
@@ -21,13 +22,31 @@ const app = express();
 
 db.connect(DB_HOST);
 
+const getUser = token => {
+  if (token) {
+    try {
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (e) {
+      throw new Error("Session invalid.");
+    }
+  }
+};
+
 // Apollo server setup
 const server = new ApolloServer({
   typeDefs,
   // Provide resolver functions for our shema fields
   resolvers,
-  context: () => {
-    return { models };
+  context: ({req}) => {
+    // get the user token form the headers
+    const token = req.headers.authorization;
+
+    // try to retrieve a user with the token
+    const user = getUser(token);
+
+    console.log(user);
+
+    return { models, user };
   }
 });
 
--- api/src/resolvers/mutation.js
+++ api/src/resolvers/mutation.js
@@ -1,3 +1,8 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { AuthenticationError, ForbiddenError } = require('apollo-server-express');
+const gravatar = require('../util/gravatar');
+
 module.exports = {
   newNote: async (parent, args, { models }) => {
     return await models.Note.create({
@@ -33,5 +38,50 @@ module.exports = {
 
     }
 
+  },
+  signUp: async (parent, { username, email, password }, { models }) => {
+
+    email = email.trim().toLowerCase();
+    const hashed = await bcrypt.hash(password, 10);
+    const avatar = gravatar(email);
+
+    try {
+      const user = await models.User.create({
+        username,
+        email,
+        avatar,
+        password: hashed
+      });
+
+      return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    } catch (e) {
+      console.log(e);
+      throw new Error('Error creating account.');
+    }
+  },
+
+  signIn: async (parent, {username, email, password}, {models}) => {
+
+    if (email) {
+      email = email.trim().toLowerCase();
+    }
+
+    const user = await models.User.findOne({
+      $or: [{email}, {username}]
+    });
+
+
+    if (!user) {
+      throw new AuthenticationError("Error signing in");
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      throw new AuthenticationError("Error signing in");
+    }
+
+    return jwt.sign({id: user._id}, process.env.JWT_SECRET);
   }
 };
